4f792dccb865e69c53b036a7b5e8983afb7fea64
Qin Wang <qin.wang2@vce.com>
2018-02-06 15:19:54 -0500

    Initial maven settings

---
+++ src/main/java/com/leetcode/java/algorithm/DummyMain.java
@@ -0,0 +1,13 @@
+package com.leetcode.java.algorithm;
+
+/**
+ * Hello world!
+ *
+ */
+public class DummyMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
